bd748888e4070039a42ad4dcf7599ec035852663
Patrick Beltrão <patrick.beltrao@outlook.com>
2025-08-28 15:37:36 -0300

    FINALIZANDO O PROJETO DO SERENATTO CAFE

--- gerador-pdf.php
+++ gerador-pdf.php
@@ -1,15 +1,21 @@
 <?php
 
 require "vendor/autoload.php";
+
 // reference the Dompdf namespace
 use Dompdf\Dompdf;
 
 // instantiate and use the dompdf class
 $dompdf = new Dompdf();
-$dompdf->loadHtml('hello world');
+
+ob_start();
+require "conteudo-pdf.php";
+$html = ob_get_clean();
+
+$dompdf->loadHtml($html);
 
 // (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'landscape');
+$dompdf->setPaper('A4');
 
 // Render the HTML as PDF
 $dompdf->render();
